323fa36464ec416b04d43b94ca94d6729808b2b0
Kgopolomo <Kgopolomo@gmail.com>
2023-07-26 11:43:05 +0200

    initial commit

---
+++ src/main/java/za/co/wiizo/wiizoapi/WiizoApiApplication.java
@@ -0,0 +1,13 @@
+package za.co.wiizo.wiizoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiizoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiizoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/wiizo/wiizoapi/WiizoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.wiizo.wiizoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiizoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
